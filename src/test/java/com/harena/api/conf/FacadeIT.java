3a05608a0494c07200f1e5b145d436b6326700e3
tanjonaaa <hei.tanjona@gmail.com>
2024-07-22 08:56:38 +0300

    chore: upgrade poja version to 16.2.1

--- src/test/java/com/harena/api/conf/FacadeIT.java
+++ src/test/java/com/harena/api/conf/FacadeIT.java
@@ -1,31 +1,25 @@
 package com.harena.api.conf;
 
-import static java.lang.Runtime.getRuntime;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
+import com.harena.api.PojaGenerated;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.BeforeAll;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import com.harena.api.PojaGenerated;
 
 @PojaGenerated
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @Slf4j
 public class FacadeIT {
 
-  
-
   @SneakyThrows
   @DynamicPropertySource
   static void configureProperties(DynamicPropertyRegistry registry) {
-    
-    
+
     new BucketConf().configureProperties(registry);
     new EmailConf().configureProperties(registry);
-    
 
     try {
       var envConfClazz = Class.forName("com.harena.api.conf.EnvConf");
