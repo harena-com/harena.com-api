b8ee2d04366f8609e3f474a5a56c5e551a45f71d
tanjonaaa <hei.tanjona@gmail.com>
2024-07-26 23:36:25 +0300

    feat: implement pagination on getFilesFromS3

--- src/main/java/com/harena/api/endpoint/rest/controller/PatrimoineController.java
+++ src/main/java/com/harena/api/endpoint/rest/controller/PatrimoineController.java
@@ -1,23 +1,27 @@
 package com.harena.api.endpoint.rest.controller;
 
 import com.harena.api.endpoint.rest.mapper.PatrimoineMapper;
+import com.harena.api.endpoint.rest.model.GetPatrimoines200Response;
 import com.harena.api.endpoint.rest.model.Patrimoine;
 import com.harena.api.service.PatrimoineService;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 @AllArgsConstructor
 public class PatrimoineController {
-    private final PatrimoineService service;
-    private final PatrimoineMapper mapper;
+  private final PatrimoineService service;
+  private final PatrimoineMapper mapper;
 
-    @GetMapping("/patrimoines")
-    public List<Patrimoine> getPatrimoines(@RequestParam(value = "page", defaultValue = "0") int page, @RequestParam(value = "page_size", defaultValue = "10") int pageSize){
-        return service.getPaginatedPatrimoines(page, pageSize).stream().map(mapper::toRest).toList();
-    }
+  @GetMapping("/patrimoines")
+  public GetPatrimoines200Response getPatrimoines(
+      @RequestParam(value = "page", defaultValue = "0") int page,
+      @RequestParam(value = "page_size", defaultValue = "10") int pageSize) {
+    List<Patrimoine> data =
+        service.getPaginatedPatrimoines(page, pageSize).stream().map(mapper::toRest).toList();
+    return new GetPatrimoines200Response().data(data);
+  }
 }
