183622cc7ef9b493f3291fe7f13e505fd44df7ba
tanjonaaa <hei.tanjona@gmail.com>
2024-07-26 22:39:23 +0300

    feat: get paginated list of patrimoines at GET /patrimoines

---
+++ src/main/java/com/harena/api/endpoint/rest/mapper/PersonneMapper.java
@@ -0,0 +1,13 @@
+package com.harena.api.endpoint.rest.mapper;
+
+import com.harena.api.endpoint.rest.model.Personne;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PersonneMapper {
+
+    public Personne toRest(school.hei.patrimoine.modele.Personne domain){
+        return new Personne()
+                .nom(domain.nom());
+    }
+}
