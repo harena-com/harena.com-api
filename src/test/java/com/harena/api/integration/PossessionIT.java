c69a53ac555c2084f6bd9ed8a9bcfb081a58a598
tendryAxel <hei.tendry@gmail.com>
2024-07-28 21:17:16 +0300

    chore: test: assert getPatrimoinePossessions contains PossessionAvecType

--- src/test/java/com/harena/api/integration/PossessionIT.java
+++ src/test/java/com/harena/api/integration/PossessionIT.java
@@ -1,15 +1,17 @@
 package com.harena.api.integration;
 
-import static com.harena.api.integration.confUtils.TestMocks.patrimoine_ilo;
+import static com.harena.api.integration.confUtils.TestMocks.*;
 import static com.harena.api.integration.confUtils.TestUtils.setupExtendedBucketComponent;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.harena.api.conf.FacadeIT;
 import com.harena.api.endpoint.rest.api.PossessionApi;
 import com.harena.api.endpoint.rest.client.ApiClient;
 import com.harena.api.endpoint.rest.client.ApiException;
+import com.harena.api.endpoint.rest.model.PossessionAvecType;
 import com.harena.api.file.ExtendedBucketComponent;
 import com.harena.api.integration.confUtils.TestUtils;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -34,7 +36,10 @@ public class PossessionIT extends FacadeIT {
   void get_paginated_possession_ok() throws ApiException {
     ApiClient apiClient = anApiClient();
     PossessionApi api = new PossessionApi(apiClient);
+    List<PossessionAvecType> data =
+        api.getPatrimoinePossessions(patrimoine_ilo().getNom(), 0, 1).getData();
 
-    assertEquals(2, api.getPatrimoinePossessions(patrimoine_ilo().getNom(), 0, 1).getData().size());
+    assertTrue(data.contains(argent_espece_de_ilo()));
+    assertTrue(data.contains(vie_courant_de_ilo()));
   }
 }
