7735b7e06de3af10f386653055cc51fb3379a809
tanjonaaa <hei.tanjona@gmail.com>
2024-07-29 08:44:21 +0300

    fix: allow Methods and Origins in CorsConf

--- src/main/java/com/harena/api/endpoint/CorsConf.java
+++ src/main/java/com/harena/api/endpoint/CorsConf.java
@@ -11,6 +11,6 @@ public class CorsConf implements WebMvcConfigurer {
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**");
+    registry.addMapping("/**").allowedMethods("*").allowedOrigins("*");
   }
 }
